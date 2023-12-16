8ded301dbc387258f58a84c0096500787d2547f3
Alejandro Garcia <thinkingmono92@gmail.com>
2023-12-16 14:43:18 -0500

    index.js | Wrap App into GithubProvider (Context). info.js | Store value passed in GithubContext 'hello' into data const using useContext hook as an example to test if global context is working. Show data into JSX return. User.js | Modify component's return text.Create GithubContext with crateContext. Declara GithubProvider component passing children object as a prop and return Github context provider with 'hello' string as a test context share value. Export GithubContext and GithubProvider. Dashboard.js | Render in JSX return Navbar, Search, Info, User, Repos components wrapper into main. Login.js | Set pre login page JSX structure.

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { GithubProvider } from './context/context';
 import { Auth0Provider } from '@auth0/auth0-react';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GithubProvider>
+      <App />
+    </GithubProvider>
   </React.StrictMode>
 );
 
