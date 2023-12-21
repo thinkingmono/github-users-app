b114cd6cde7dd3dbcd9a688e84b436add2b5d074
Alejandro Garcia <thinkingmono92@gmail.com>
2023-12-20 20:51:30 -0500

    .env | Add generate_sourcemap variable set to false to remove fusioncharts warning. Navbar.js | Adding wrapper to Navbar return.user state variable to store search's field value. Destructure values from global context. handleSubmit function to call searchGithubUser function which fetch git hub users data from the server. handler asociates with form's submit button. Return JSX search field and error structure. Input onChange handler with setUser state variable change. Hide search button if query is loading and if app reach query limit. context.js | request, isLoading and error state variables declaration. checkRequest function to send a get request to the server to check queries limit. Use axios libary. If there are no queries show notification error. Error handling with catch. toggleError function to show or hide the error. searchGithubUser to fetch search user data, followers and repos. Check if there are queries still available and show or hide loader while query fetch. useEffect hook to call checkRequest when page loads. share request, error, searchGithubUser and isLoading values to context provider. So they are available in the app. Deashboard.js | Destructure isLoading from context. Check if query is loading if it is render navbar, search field and loader img.

--- src/context/context.js
+++ src/context/context.js
@@ -12,8 +12,51 @@ const GithubProvider = ({ children }) => {
     const [githubUser, setGithubUser] = useState(mockUser);
     const [repos, setRepos] = useState(mockRepos);
     const [followers, setFollowers] = useState(mockFollowers);
+
+    const [request, setRequest] = useState(0);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState({ show: false, msg: '' });
+
+    const checkRequest = () => {
+        axios(`${rootUrl}/rate_limit`).then(({ data }) => {
+            let { rate: { remaining } } = data;
+            // remaining = 0; test value
+            setRequest(remaining);
+            if (remaining === 0) {
+                toggleError(true, 'sorry, you have exceeded your hourly rate limit');
+            }
+        }).catch((error) => {
+            console.log(error);
+        })
+    }
+
+    const toggleError = (show = false, msg = '') => {
+        setError({ show, msg });
+    }
+
+    const searchGithubUser = async (user) => {
+        toggleError();
+        setIsLoading(true);
+        const response = await axios(`${rootUrl}/users/${user}`).catch((error) => console.log(error));
+        if (response) {
+            setGithubUser(response.data);
+            console.log(response.data);
+            const { login, followers_url } = response.data;
+            //https://api.github.com/users/john-smilga/repos?per_page=100
+            axios(`${rootUrl}/users/${login}/repos?per_page=100`).then((response) => setRepos(response.data));
+            //https://api.github.com/users/john-smilga/followers
+            axios(`${followers_url}?per_page=100`).then((response) => setFollowers(response.data));
+        } else {
+            toggleError(true, 'there are no user with that username');
+        }
+        checkRequest();
+        setIsLoading(false);
+    }
+
+    useEffect(checkRequest, []);
+
     return (
-        <GithubContext.Provider value={{ githubUser, repos, followers }}>{children}</GithubContext.Provider>
+        <GithubContext.Provider value={{ githubUser, repos, followers, request, error, searchGithubUser, isLoading }}>{children}</GithubContext.Provider>
     )
 }
 
